79476345d4ae3f231f440e473828ad26fd622e56
xXGreGXx <gregloha@gmail.com>
2019-11-04 13:51:13 +0100

    initial commit contains definitions and function name

---
+++ main.py
@@ -0,0 +1,9 @@
+import time
+
+# xDays [monday = 0, tuesday = 1, wednesday = 2, thursday = 3, friday = 4, saturday = 5, sunday = 6,]
+# nD every nth xDay of nM
+# nM every nth month for nY
+# nY for n years
+
+def getDates(nD, xDay, nM, nY):
+    
